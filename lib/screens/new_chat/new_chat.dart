c28b0dd225dac59cb0632039f71a6f038313389a
Raman <24293550+ramank775@users.noreply.github.com>
2022-05-20 23:38:31 +0530

    feat: delay contact permission till new chat page

--- lib/screens/new_chat/new_chat.dart
+++ lib/screens/new_chat/new_chat.dart
@@ -1,4 +1,5 @@
 import 'package:share_plus/share_plus.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'package:vartalap/config/config_store.dart';
 import 'package:vartalap/models/chat.dart';
 import 'package:vartalap/models/user.dart';
@@ -18,6 +19,7 @@ class NewChatState extends State<NewChatScreen>
   late Future<List<User>> _contacts;
   late Future<List<Chat>> _groups;
   late TabController _tabController;
+  late Future<PermissionStatus> _fPermission;
   bool _openSearch = false;
   @override
   void initState() {
@@ -25,6 +27,7 @@ class NewChatState extends State<NewChatScreen>
     _tabController = TabController(length: 2, vsync: this);
     _contacts = UserService.getUsers();
     _groups = ChatService.getGroups();
+    _fPermission = Permission.contacts.status;
   }
 
   @override
@@ -40,74 +43,18 @@ class NewChatState extends State<NewChatScreen>
         body: TabBarView(
           controller: _tabController,
           children: [
-            contacts(context),
-            groups(),
+            contacts(),
+            GroupList(
+              groups: _groups,
+              onTap: onGroupTap,
+            ),
           ],
         ));
   }
 
-  Widget contacts(BuildContext context) {
-    return FutureBuilder<Iterable<User>>(
-      future: _contacts,
-      builder: (context, snapshot) {
-        switch (snapshot.connectionState) {
-          case ConnectionState.none:
-            return Center(
-              child: CircularProgressIndicator(),
-            );
-          case ConnectionState.active:
-          case ConnectionState.waiting:
-            return Center(
-              child: CircularProgressIndicator(),
-            );
-          case ConnectionState.done:
-            if (snapshot.hasError) {
-              return Center(
-                child: Text('Error: ${snapshot.error}'),
-              );
-            }
-        }
-        List<dynamic> data = [];
-        data.addAll(snapshot.data!);
-        data.add(ListTile(
-          leading: Container(
-            padding: const EdgeInsets.all(8.0),
-            child: Icon(
-              Icons.share,
-              color: Theme.of(context).textTheme.bodyText1?.color,
-            ),
-          ),
-          title: Text('Invite friends',
-              style: TextStyle(
-                fontSize: 18.0,
-                fontWeight: FontWeight.bold,
-              )),
-          onTap: () {
-            Share.share(ConfigStore().get('share_message'));
-          },
-        ));
-
-        return ListView.builder(
-            itemCount: data.length,
-            itemBuilder: (context, i) {
-              if (i > data.length - 2) {
-                return data[i];
-              }
-              return ContactItem(
-                  user: data.elementAt(i),
-                  onProfileTap: () =>
-                      {}, // onTapProfileContactItem( context, snapshot.data.elementAt(i))
-                  onTap: (User user) async {
-                    Navigator.of(context).pop(user);
-                  });
-            });
-      },
-    );
-  }
-
-  Widget groups() {
-    return FutureBuilder<Iterable<Chat>>(
-      future: _groups,
+  Widget contacts() {
+    return FutureBuilder<PermissionStatus>(
+      future: _fPermission,
       builder: (context, snapshot) {
         switch (snapshot.connectionState) {
           case ConnectionState.none:
@@ -130,47 +77,18 @@ class NewChatState extends State<NewChatScreen>
               );
             }
         }
-        List<dynamic> data = [];
-        data.add(ListTile(
-          leading: Container(
-            decoration: BoxDecoration(
-              //color: fabBgColor,
-              borderRadius: BorderRadius.circular(20),
-            ),
-            padding: const EdgeInsets.all(4.0),
-            child: Icon(
-              Icons.group,
-              size: 32.0,
-              color: Colors.grey,
-            ),
-          ),
-          title: Text('New group',
-              style: TextStyle(
-                fontSize: 18.0,
-                fontWeight: FontWeight.bold,
-              )),
-          onTap: () {
-            Navigator.of(context).pushReplacementNamed('/new-group');
-          },
-        ));
-
-        data.addAll(snapshot.data!);
-        return ListView.builder(
-            itemCount: data.length,
-            itemBuilder: (context, i) {
-              if (i < 1) {
-                return data[i];
-              }
-              Chat chat = data.elementAt(i);
-              ChatPreview preview =
-                  ChatPreview(chat.id, chat.title, chat.pic, '', 0, 0);
-              preview.type = chat.type;
-              return ChatPreviewWidget(
-                preview,
-                onGroupTap,
-                onGroupTap,
-              );
+        if (snapshot.data == PermissionStatus.granted) {
+          return ContactList(contacts: _contacts);
+        } else {
+          return ContactPermissionDisclosure(onSkip: () {
+            _tabController.index = 1;
+          }, onAllow: () {
+            setState(() {
+              _fPermission = Permission.contacts.status;
+              _contacts = UserService.getUsers(sync: true);
             });
+          });
+        }
       },
     );
   }
@@ -250,21 +168,18 @@ class NewChatState extends State<NewChatScreen>
         child: Icon(
           Icons.arrow_back,
           size: 24.0,
-          //color: Colors.white,
         ),
       ),
       titleSpacing: 0,
       automaticallyImplyLeading: false,
       title: TextField(
         style: TextStyle(
-          //color: Colors.white,
           fontSize: 20.0,
         ),
         decoration: InputDecoration(
           border: InputBorder.none,
           hintText: "Search",
           hintStyle: TextStyle(
-            //color: Colors.white,
             fontSize: 20.0,
           ),
         ),
@@ -280,6 +195,237 @@ class NewChatState extends State<NewChatScreen>
         },
       ),
       actions: [],
+    );
+  }
+}
+
+class ContactPermissionDisclosure extends StatelessWidget {
+  const ContactPermissionDisclosure(
+      {Key? key, required Function onSkip, required Function onAllow})
+      : _onSkip = onSkip,
+        _onAllow = onAllow,
+        super(key: key);
+  final Function _onSkip;
+  final Function _onAllow;
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(15.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Column(
+            children: [
+              Text(
+                "Contact Permission Required",
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ],
+          ),
+          Icon(
+            Icons.contacts_rounded,
+            size: 70,
+          ),
+          Column(
+            children: [
+              Text(
+                "Vartalap need to access your contacts, to provide you with the list of users using our services in your contact.",
+              ),
+              Text(""),
+              Text(
+                "All the information expect phone number are stored on your device only and are not collected in any channel. "
+                "Contacts syncing happen at periodically, inorder to keep the contact upto date.",
+                style: TextStyle(fontSize: 13),
+              ),
+              Text(""),
+              Text(
+                "Note: Your contact's phone number are not stored on servers, only used for the syncing your contact list.",
+                style: TextStyle(
+                  fontSize: 12,
+                  fontStyle: FontStyle.italic,
+                ),
+              )
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              TextButton(
+                  onPressed: () {
+                    _onSkip();
+                  },
+                  child: Text('Skip')),
+              TextButton(
+                  onPressed: () async {
+                    final permission = await Permission.contacts.request();
+                    if (permission == PermissionStatus.granted) {
+                      _onAllow();
+                    }
+                  },
+                  child: Text('Allow'))
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class GroupList extends StatelessWidget {
+  const GroupList({
+    Key? key,
+    required Future<List<Chat>> groups,
+    required Function(Chat ch) onTap,
+  })  : _groups = groups,
+        _onTap = onTap,
+        super(key: key);
+
+  final Future<List<Chat>> _groups;
+  final Function(Chat) _onTap;
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<Iterable<Chat>>(
+      future: _groups,
+      builder: (context, snapshot) {
+        switch (snapshot.connectionState) {
+          case ConnectionState.none:
+            return Center(
+              child: CircularProgressIndicator(
+                valueColor: new AlwaysStoppedAnimation<Color>(Colors.grey),
+              ),
+            );
+          case ConnectionState.active:
+          case ConnectionState.waiting:
+            return Center(
+              child: CircularProgressIndicator(
+                valueColor: new AlwaysStoppedAnimation<Color>(Colors.grey),
+              ),
+            );
+          case ConnectionState.done:
+            if (snapshot.hasError) {
+              return Center(
+                child: Text('Error: ${snapshot.error}'),
+              );
+            }
+        }
+        List<dynamic> data = [];
+        data.add(ListTile(
+          leading: Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+            ),
+            padding: const EdgeInsets.all(4.0),
+            child: Icon(
+              Icons.group,
+              size: 32.0,
+              color: Colors.grey,
+            ),
+          ),
+          title: Text('New group',
+              style: TextStyle(
+                fontSize: 18.0,
+                fontWeight: FontWeight.bold,
+              )),
+          onTap: () {
+            Navigator.of(context).pushReplacementNamed('/new-group');
+          },
+        ));
+
+        data.addAll(snapshot.data!);
+        return ListView.builder(
+            itemCount: data.length,
+            itemBuilder: (context, i) {
+              if (i < 1) {
+                return data[i];
+              }
+              Chat chat = data.elementAt(i);
+              ChatPreview preview =
+                  ChatPreview(chat.id, chat.title, chat.pic, '', 0, 0);
+              preview.type = chat.type;
+              return ChatPreviewWidget(
+                preview,
+                _onTap,
+                _onTap,
+              );
+            });
+      },
+    );
+  }
+}
+
+class ContactList extends StatelessWidget {
+  const ContactList({
+    Key? key,
+    required Future<List<User>> contacts,
+  })  : _contacts = contacts,
+        super(key: key);
+
+  final Future<List<User>> _contacts;
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<Iterable<User>>(
+      future: _contacts,
+      builder: (context, snapshot) {
+        switch (snapshot.connectionState) {
+          case ConnectionState.none:
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          case ConnectionState.active:
+          case ConnectionState.waiting:
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          case ConnectionState.done:
+            if (snapshot.hasError) {
+              return Center(
+                child: Text('Error: ${snapshot.error}'),
+              );
+            }
+        }
+        List<dynamic> data = [];
+        data.addAll(snapshot.data!);
+        data.add(ListTile(
+          leading: Container(
+            padding: const EdgeInsets.all(8.0),
+            child: Icon(
+              Icons.share,
+              color: Theme.of(context).textTheme.bodyText1?.color,
+            ),
+          ),
+          title: Text(
+            'Invite friends',
+            style: TextStyle(
+              fontSize: 18.0,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          onTap: () {
+            Share.share(ConfigStore().get('share_message'));
+          },
+        ));
+
+        return ListView.builder(
+          itemCount: data.length,
+          itemBuilder: (context, i) {
+            if (i > data.length - 2) {
+              return data[i];
+            }
+            return ContactItem(
+                user: data.elementAt(i),
+                onProfileTap: () => {},
+                onTap: (User user) async {
+                  Navigator.of(context).pop(user);
+                });
+          },
+        );
+      },
     );
   }
 }
--- lib/screens/startup/startup.dart
+++ lib/screens/startup/startup.dart
@@ -8,7 +8,6 @@ import 'package:permission_handler/permission_handler.dart';
 import 'package:vartalap/theme/theme.dart';
 import 'package:vartalap/widgets/Inherited/config_provider.dart';
 import 'package:vartalap/widgets/app_logo.dart';
-import 'package:vartalap/widgets/rich_message.dart';
 
 class StartupScreen extends StatelessWidget {
   const StartupScreen({Key? key}) : super(key: key);
@@ -18,42 +17,8 @@ class StartupScreen extends StatelessWidget {
     var value = await Permission.contacts.status;
     if (value.isGranted) {
       onContactPermissionGranted(context);
-      onNext(context);
-    } else {
-      final dialog = AlertDialog(
-        title: Icon(
-          Icons.contact_page_rounded,
-          size: 40,
-        ),
-        titleTextStyle: TextStyle(fontSize: 14),
-        content: RichMessage(
-          "To help you connect with friends and family, allow ${configStore.packageInfo.appName} access to your contacts.",
-          TextStyle(
-            fontSize: 16,
-          ),
-        ),
-        actions: [
-          TextButton(
-            child: Text("Not Now"),
-            onPressed: () {
-              Navigator.of(context).pop();
-              onNext(context);
-            },
-          ),
-          TextButton(
-              child: Text("Continue"),
-              onPressed: () async {
-                Navigator.of(context).pop();
-                final value = await Permission.contacts.request();
-                if (value.isGranted) {
-                  onContactPermissionGranted(context);
-                  onNext(context);
-                }
-              }),
-        ],
-      );
-      showDialog(context: context, builder: (BuildContext context) => dialog);
     }
+    onNext(context);
   }
 
   void onNext(BuildContext context) {
